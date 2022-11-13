453d27714660543a959928d1e880ebba8a6f4c2a
hy-cho <hyunbum.cho@mail.mcgill.ca>
2022-11-12 23:06:09 -0500

    removed duplciate food names

--- frontend/food_data/get_food_id.py
+++ frontend/food_data/get_food_id.py
@@ -5,9 +5,11 @@ foundataion_food_filename = "foundation_food.csv"
 branded_filename = "branded_food.csv"
 our_foods_filename = "our_foods.csv"
 food_dict = {}
+# do not add duplicate food names.
+added_food_names = {}
 data = []
 
-# store id, name of all foods in dictionary
+# store id, name of all foods in dictionary. food_dict[id] = [name]
 with open(food_filename, newline="", encoding="utf-8") as csvfile:
     file_reader = csv.reader(csvfile, quoting=csv.QUOTE_ALL)
     next(csvfile)
@@ -20,11 +22,19 @@ with open(our_foods_filename, 'w') as writefile:
         file_reader = csv.reader(csvfile, quoting=csv.QUOTE_ALL)
         next(csvfile)
         for row in file_reader:
-            data = [row[0], food_dict[row[0]]]
-            file_writer.writerow(data)
+            food_id = row[0]
+            food_name = food_dict[food_id]
+            if not(food_name in added_food_names):
+                data = [food_id, food_name]
+                added_food_names[food_name] = None
+                file_writer.writerow(data)
     with open(branded_filename, newline="", encoding="utf-8") as csvfile:
         file_reader = csv.reader(csvfile, quoting=csv.QUOTE_ALL)
         next(csvfile)
         for row in file_reader:
-            data = [row[0], food_dict[row[0]]]
-            file_writer.writerow(data)
+            food_id = row[0]
+            food_name = food_dict[food_id]
+            if not(food_name in added_food_names):
+                data = [food_id, food_name]
+                added_food_names[food_name] = None
+                file_writer.writerow(data)
