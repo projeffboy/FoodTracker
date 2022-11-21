19b2198df4c31fd33bd0e5080af3e3213d1580e0
OxfordZhuo <zhuojunlin0106@gmail.com>
2022-11-20 21:10:36 -0500

    added route function

--- backend/foodtracker/root/foodtracker/app.py
+++ backend/foodtracker/root/foodtracker/app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, request, make_response
+from werkzeug.exceptions import BadRequest
 from utils import *
+from PIL import Image
+import io
+import json
 
 app = Flask(__name__)
 
@@ -10,6 +14,53 @@ def hello_world():  # put application's code here
     food = Food(output_from_ml[0],output_from_ml[1],output_from_ml[2],output_from_ml[3])
     get_food_info(food)
     return str(food)
+
+
+@app.route('/image_query', methods=['POST'])
+def handler():
+
+    def extract_image(request_in):
+        if 'file' not in request_in.files:
+            raise BadRequest("Missing file (image/jpeg).")
+        file = request_in.files['file']
+        if file.filename == '':
+            raise BadRequest("File name is invalid.")
+
+        return file
+
+    file = extract_image(request)
+    image = Image.open(io.BytesIO(file.read()))
+    result = [[]]
+    # TODO
+    '''
+    Send the image to the ML function, and obtain a list of lists
+    result = ml_func(image)
+    '''
+
+    def parse_result(r):
+        json_content = []
+        for x in r:
+            food = Food(x[0], x[1], x[2], x[3])
+            get_food_info(food)
+            food_dict = {'name': food.get_name(),
+                         'id': food.get_id(),
+                         'default_quantity': food.get_default_quantity(),
+                         'deafult_quantity_unit': food.get_deafult_quantity_unit(),
+                         'calories': food.get_calories(),
+                         'protien': food.get_protien(),
+                         'fat': food.fat(),
+                         'sugar': food.sugar(),
+                         'fiber': food.fiber(),
+                         'calcium': food.calcium()}
+            json_content.append(food_dict)
+
+        return json.dumps(json_content)
+
+    res_json = parse_result(result)
+    response = make_response(res_json, 200)
+    response.headers['Content-type'] = 'application/json'
+
+    return response
 
 
 if __name__ == '__main__':
--- backend/foodtracker/root/foodtracker/models.py
+++ backend/foodtracker/root/foodtracker/models.py
@@ -1,4 +1,3 @@
-
 class Food:
     # important!!! For some food, default quantity (e.g. weight) does not exist. For example, 
     # "Apples, red delicious, with skin, raw" does not have mass in the database. In this case, we must 
@@ -10,53 +9,74 @@ class Food:
         self.id = id
         # default portion of food
         self.default_quantity = default_quantity
-        self.deafult_quantity_unit = default_unit
+        self.default_quantity_unit = default_unit
         self.calories = None
         self.protien = None
         self.fat = None
         self.sugar = 0
         self.fiber = None
         self.calcium = None
+
     def get_name(self):
         return self.name
+
     def get_id(self):
         return str(self.id)
+
     def get_calories(self):
         return self.calories
+
     def set_calories(self, calories):
         self.calories = calories
+
     def get_default_quantity(self):
         return self.calories
+
     def set_default_quantity(self, default_quantity):
         self.default_quantity = default_quantity
+
     def get_deafult_quantity_unit(self):
-        return self.deafult_quantity_unit
+        return self.default_quantity_unit
+
     def set_deafult_quantity_unit(self, deafult_quantity_unit):
-        self.deafult_quantity_unit = deafult_quantity_unit
+        self.default_quantity_unit = deafult_quantity_unit
+
     def set_protien(self, protien):
         self.protien = protien
+
     def get_protien(self):
         return self.protien
+
     def set_fat(self, fat):
         self.fat = fat
+
     def get_fat(self):
         return self.fat
+
     def set_sugar(self, sugar):
         self.sugar = sugar
+
     def get_sugar(self):
         return self.sugar
+
     def set_fiber(self, fiber):
         self.fiber = fiber
+
     def get_fiber(self):
         return self.fiber
+
     def set_calcium(self, calcium):
         self.calcium = calcium
+
     def get_calcium(self):
         return self.calcium
+
     def set_sodium(self, sodium):
         self.sodium = sodium
+
     def get_sodium(self):
         return self.sodium
+
     def __str__(self):
         return f"{self.name} \
             has energy: {self.calories}{Calorie.get_unit()}, \
@@ -66,62 +86,83 @@ class Food:
             fiber: {self.fiber}{Fiber.get_unit()}, \
             calcium: {self.calcium}{Calcium.get_unit()}, \
             sodium: {self.sodium}{Sodium.get_unit()}, \
-            per {self.default_quantity}{self.deafult_quantity_unit}."
+            per {self.default_quantity}{self.default_quantity_unit}."
+
 
 # very basic calorie info.
 class Calorie:
     def get_unit():
         return 'kcal'
+
     def get_daily_consumption(gender="male"):
-        return 2500 if (gender=="male") else 2000
-    def get_nutrientId(): 
+        return 2500 if (gender == "male") else 2000
+
+    def get_nutrientId():
         # returns the id that is used to identify energy in database (energy or Atwater Factors) found in nutrients.csv
         return 1062, 2047, 2048
+
 
 class Protien:
     def get_unit():
         return "g"
-    def get_daily_consumption(weight = 70):
+
+    def get_daily_consumption(weight=70):
         return weight * 0.8
+
     def get_nutrientId():
         return 1003
+
 
 class Fat:
     def get_unit():
         return "g"
+
     def get_daily_consumption(gender="male"):
-        return Calorie.get_daily_consumption(gender) * 66/2000 # 35% of calorie intake as per USDA
+        return Calorie.get_daily_consumption(gender) * 66 / 2000  # 35% of calorie intake as per USDA
+
     def get_nutrientId():
         return 1004
+
 
 class Sugar:
     def get_unit():
         return "g"
+
     def get_daily_consumption(gender="male"):
-        return 36 if (gender=="male") else 25
+        return 36 if (gender == "male") else 25
+
     def get_nutrientId():
         return 1010, 1011, 1012, 1063
+
 
 class Fiber:
     def get_unit():
         return "g"
+
     def get_daily_consumption():
         return 25
+
     def get_nutrientId():
         return 1079
-    
+
+
 class Calcium:
     def get_unit():
         return "mg"
+
     def get_daily_consumption():
         return 1000
+
     def get_nutrientId():
         return 1087
+
 
 class Sodium:
     def get_unit():
         return "mg"
+
     def get_daily_consumption():
         return 2300
+
     def get_nutrientId():
         return 1093
